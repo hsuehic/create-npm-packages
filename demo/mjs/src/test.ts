efb8eabdf3d372b728a274eb5b36314a61db2dc7
Xiaowei Xue <xiaowei.xue@shopee.com>
2023-08-10 16:49:53 +0800

    chore: update module settings

--- demo/mjs/src/test.ts
+++ demo/mjs/src/test.ts
@@ -1,7 +1,30 @@
-import { exec, setPackageJsonField } from 'cnp-utils';
+import * as path from 'path';
+
+import {
+  Singleton,
+  download,
+  download1,
+  exec,
+  setPackageJsonField,
+} from 'cnp-utils';
 
 void exec`date`.then(stdout => {
-  console.log(stdout);
+  stdout.split('');
 });
 
 void setPackageJsonField('name', 'test');
+
+void download(
+  'https://github.com/microsoft/TypeScript/archive/refs/tags/v5.1.5.zip',
+  path.resolve(__dirname, './main.zip')
+);
+
+void download1(2, 3);
+
+const singleton: Singleton<string> = {
+  name: 'test',
+  getInstance() {
+    return this.name;
+  },
+};
+singleton.getInstance<number>();
