627ddc09a97bfb57d5ee620d35b9e755fc6cad1e
Xiaowei Xue <xiaowei.xue@shopee.com>
2023-08-20 02:02:04 +0800

    fix: typo

--- packages/cnp-lint/src/contant.ts
+++ packages/cnp-lint/src/contant.ts
@@ -1,7 +1,7 @@
 // constants for managing dependencies for lint
 
 export const ESLINT_CONFIG_PACKAGE = 'eslint-config-cnp';
-export const STYLELINT_CONFIG_PACKAGE = 'stylelint-confg-cnp';
+export const STYLELINT_CONFIG_PACKAGE = 'stylelint-config-cnp';
 export const COMMITLINT_CONFIG_PACKAGE = '@commitlint/config-conventional';
 export const NPMPACKAGEJSONLINT_CONFIG_PACKAGE =
   'npm-package-json-lint-config-default';
